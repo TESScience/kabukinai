f7576155271aba45e3d2d768767c498fff609a10
John Doty <jpd@noqsi.com>
2017-03-08 11:57:57 -0700

    Working on panel index logic

--- src/libraries/star_data/star_data.h
+++ src/libraries/star_data/star_data.h
@@ -26,17 +26,21 @@ typedef struct {
 } dimensions;
 
 typedef struct {
-    dimensions single_panel_pixel_dimensions; // Size of a single panel in pixels, for debugging
+    dimensions single_panel_pixel_dimensions;
     dimensions panel_indices_dimensions;
-    minmax x_pixels;
-    minmax y_pixels;
+    minmax x_pixels;  // Size of a single panel in pixels, for debugging
+    minmax y_pixels;  // Size of a single panel in pixels, for debugging
 } star_meta_data;
 
 #ifdef __NVCC__
 __host__ __device__
 #endif
-inline long panel_index_lookup(const long x, const long y, const dimensions panel_indices_dimensions) {
-    return x * panel_indices_dimensions.y_dimension + y;
+inline int panel_index_lookup(const float x, const float y, const star_meta_data metadata) {
+	const float panel_x = x / metadata.single_panel_pixel_dimensions.x_dimension + 1;
+	if( panel_x < 0 || panel_x >= metadata.panel_indices_dimensions.x_dimension) return -1;
+	const float panel_y = y / metadata.single_panel_pixel_dimensions.y_dimension + 1;
+	if(panel_y < 0 || panel_y >= metadata.panel_indices_dimensions.y_dimension) return -1;
+    return ((int) y) * panel_indices_dimensions.x_dimension + ((int) x);
 }
 
 /*
