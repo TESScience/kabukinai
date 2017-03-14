797b5d8f42c07962ac6aa683c1e5fffc595e80a5
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2017-03-14 12:36:24 -0400

    Parsing star data from TSV is functional

--- src/libraries/star_data/parse_star_data_from_tsv.cpp
+++ src/libraries/star_data/parse_star_data_from_tsv.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <math.h>
 
-class ParseStarDataException : public std::exception {
+class StarDataException : public std::exception {
 public:
     /** Constructor (C strings).
      *  @param message C-style string error message.
@@ -11,14 +11,14 @@ public:
      *                 Hence, responsibility for deleting the char* lies
      *                 with the caller.
      */
-    explicit ParseStarDataException(const char *message) :
+    explicit StarDataException(const char *message) :
             msg_(message) {
     }
 
     /** Destructor.
      * Virtual to allow for subclassing.
      */
-    virtual ~ParseStarDataException() throw() {}
+    virtual ~StarDataException() throw() {}
 
     /** Returns a pointer to the (constant) error description.
      *  @return A pointer to a const char*. The underlying memory
@@ -44,8 +44,8 @@ std::vector<star> parse_star_vector_from_tsv(const char *file_name) {
         const char *cline = line.c_str();
         star new_star;
         int parsed_arguments = sscanf(cline, "%g %g %g %g %g %g %g %g %g %g",
-                                      &new_star.x,
-                                      &new_star.y,
+                                      &new_star.point.x,
+                                      &new_star.point.y,
                                       &intensities[0], &intensities[1],
                                       &intensities[2], &intensities[3],
                                       &intensities[4], &intensities[5],
@@ -54,7 +54,7 @@ std::vector<star> parse_star_vector_from_tsv(const char *file_name) {
             char error_message_data[1024];
             snprintf(error_message_data, sizeof(error_message_data),
                      "Could not parse line of TSV:\n\n %s", cline);
-            throw ParseStarDataException(error_message_data);
+            throw StarDataException(error_message_data);
         }
         for (int color_index = 0; color_index < STAR_COLORS; ++color_index)
             new_star.intensities[color_index] = intensities[color_index];
@@ -68,23 +68,38 @@ int star_data_from_vector(star_data *data,
                           const dimensions image_dimensions,
                           const dimensions single_panel_pixel_dimensions) {
 
-    // TODO: error on negative
-    data->metadata.single_panel_pixel_dimensions = single_panel_pixel_dimensions;
-    data->metadata.image_dimensions = image_dimensions;
-    data->metadata.panel_indices_dimensions.x_dimension =
+    data->meta_data.single_panel_pixel_dimensions = single_panel_pixel_dimensions;
+    data->meta_data.image_dimensions = image_dimensions;
+    data->meta_data.panel_indices_dimensions.x_dimension =
             (int) ceil(image_dimensions.x_dimension / single_panel_pixel_dimensions.x_dimension + 2);
-    data->metadata.panel_indices_dimensions.y_dimension =
+    data->meta_data.panel_indices_dimensions.y_dimension =
             (int) ceil(image_dimensions.y_dimension / single_panel_pixel_dimensions.y_dimension + 2);
 
-    const unsigned long number_of_panels = (const unsigned long) (data->metadata.panel_indices_dimensions.x_dimension *
-                                                                  data->metadata.panel_indices_dimensions.y_dimension);
+    const star_meta_data &meta_data = data->meta_data;
+    const unsigned long number_of_panels = (const unsigned long) (meta_data.panel_indices_dimensions.x_dimension *
+                                                                  meta_data.panel_indices_dimensions.y_dimension);
 
     std::vector<std::vector<star>> panel_intermediate_data(number_of_panels);
     for (const star &star_data : stars) {
-        // TODO: Error if star out of bounds
-        panel_intermediate_data.at(
-                (unsigned long) panel_index_lookup(star_data.x, star_data.y, data->metadata)).push_back(
-                star_data);
+        const int panel_index = panel_index_lookup(star_data.point.x, star_data.point.y, meta_data);
+        if (panel_index < 0) {
+            const point_2d panel_indices = compute_panel_2d_indices(star_data.point.x, star_data.point.y, meta_data);
+            char error_message_data[1024];
+            snprintf(error_message_data, sizeof(error_message_data),
+                     "Star coordinates out of bounds: {\"x\": %g, \"y\": %g}\n"
+                             "Indices of panel for point: {\"x\": %g, \"y\": %g}\n"
+                             "Single panel pixel dimensions: {\"x_dimension\": %i, \"y_dimension\": %i}\n"
+                             "Panel indices dimensions: {\"x_dimension\": %i, \"y_dimension\": %i}\n",
+                     star_data.point.x, star_data.point.y,
+                     panel_indices.x,
+                     panel_indices.y,
+                     meta_data.single_panel_pixel_dimensions.x_dimension,
+                     meta_data.single_panel_pixel_dimensions.y_dimension,
+                     meta_data.panel_indices_dimensions.x_dimension,
+                     meta_data.panel_indices_dimensions.y_dimension);
+            throw StarDataException(error_message_data);
+        }
+        panel_intermediate_data.at((unsigned long) panel_index).push_back(star_data);
     }
     data->panel_indices = (long *) calloc(sizeof(long), number_of_panels + 1);
     data->stars = (star *) calloc(sizeof(star), stars.size());
--- test/libraries/psf/benchmark.c
+++ test/libraries/psf/benchmark.c
@@ -7,7 +7,7 @@ int main() {
     const double start_time = (double) clock() / CLOCKS_PER_SEC;
     double total = 0;
     srand(0);
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < 200000; ++i)
         total += cauchy_psf(10 * rand() - 50, 10 * rand() - 5, 10 * rand());
     printf("total: %g\n", total);
     const double end_time = (double) clock() / CLOCKS_PER_SEC;
