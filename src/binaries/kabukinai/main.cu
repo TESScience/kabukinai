7bf3f6e293c54d9ac5a7ca965e54a7729695102d
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2017-03-23 12:21:20 -0400

    Getting rid of printf debugging, using all threads

--- src/binaries/kabukinai/main.cu
+++ src/binaries/kabukinai/main.cu
@@ -28,6 +28,7 @@ int main(const int argc, char *argv[]) {
 
     parse_star_data_from_tsv(&star_data_from_file, argv[2], image_dimensions, single_panel_pixel_dimensions);
 
+
     const size_t image_size = sizeof(float) * image_dimensions.x_dimension * image_dimensions.y_dimension;
 
     float *pixels;
@@ -37,46 +38,22 @@ int main(const int argc, char *argv[]) {
     star *stars;
     const size_t stars_size = sizeof(star) * NUMBER_OF_STARS(star_data_from_file);
 
-    printf("panel x: %i, panel y: %i\n\n",
-           star_data_from_file.meta_data.panel_indices_dimensions.x_dimension,
-           star_data_from_file.meta_data.panel_indices_dimensions.y_dimension);
-
-    printf("***********NUMBER_OF_STARS = %ld***********\n",
-           stars_size / sizeof(star)); // DEBUG
-    for (int i = 2000; i < 2010; ++i) {
-        printf("**********star %d %f %f %f*********\n",
-               i,
-               star_data_from_file.stars[i].point.x,
-               star_data_from_file.stars[i].point.y,
-               star_data_from_file.stars[i].intensities[0]);
-    }
     PANIC_ON_BAD_CUDA_STATUS(cudaMalloc((void **) &stars, stars_size));
     PANIC_ON_BAD_CUDA_STATUS(cudaMemcpy(stars, star_data_from_file.stars, stars_size, cudaMemcpyHostToDevice));
 
     int *panel_indices;
     const size_t panel_indices_size = sizeof(int) * NUMBER_OF_PANEL_INDICES(star_data_from_file);
 
-    printf("***********NUMBER_OF_PANEL_INDICES = %ld***********\n", panel_indices_size / sizeof(int)); // DEBUG
-    for (int i = 2000; i < 2010; ++i) {
-        printf("**********panel_index %d %d*********\n", i,
-               star_data_from_file.panel_indices[i]);
-    }
-
     PANIC_ON_BAD_CUDA_STATUS(cudaMalloc((void **) &panel_indices, panel_indices_size));
-    PANIC_ON_BAD_CUDA_STATUS(cudaMemcpy(
-            panel_indices,
-            star_data_from_file.panel_indices,
-            panel_indices_size,
-            cudaMemcpyHostToDevice));
+    PANIC_ON_BAD_CUDA_STATUS(cudaMemcpy(panel_indices,
+                                        star_data_from_file.panel_indices,
+                                        panel_indices_size,
+                                        cudaMemcpyHostToDevice));
 
-//	dim3 dgrid(image_dimensions.y_dimension/single_panel_pixel_dimensions.y_dimension,
-    // image_dimensions.x_dimension/single_panel_pixel_dimensions.x_dimension);
+    dim3 dgrid(image_dimensions.y_dimension / single_panel_pixel_dimensions.y_dimension,
+               image_dimensions.x_dimension / single_panel_pixel_dimensions.x_dimension);
 
-    dim3 dgrid(1, 1);    // DEBUG
-
-//	dim3 dblock(single_panel_pixel_dimensions.y_dimension, single_panel_pixel_dimensions.x_dimension);
-
-    dim3 dblock(1, 1);    // DEBUG
+    dim3 dblock(single_panel_pixel_dimensions.y_dimension, single_panel_pixel_dimensions.x_dimension);
 
     sum_intensities_for_pixel << < dgrid, dblock >> > (pixels, stars, panel_indices, star_data_from_file.meta_data);
 
